80775420b718f89716ed75b0c66beee778c9d47c
jitae-moon <jitae.moon@icloud.com>
2023-02-03 13:48:15 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 600 ms, Memory: 38292 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,21 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int t;
-		t = Integer.valueOf(sc.nextLine());
-		
-		for (int i = 0; i < t; i++) {
-			String[] strArr = sc.nextLine().split(" ");
-			for (int j = 0; j < strArr.length; j++) {
-				String str = (String) new StringBuilder(strArr[j]).reverse().toString();
-				System.out.print(str + " ");
+		t = Integer.valueOf(br.readLine());
+
+		while (t-- > 0) {
+			String str = br.readLine() + " ";
+			Stack<Character> stack = new Stack<>();
+			for (char ch: str.toCharArray()) {
+				if (ch != ' '  && ch != '\n') {
+					stack.push(ch);
+				} else {
+					if (stack.empty()) {
+						break;
+					} else {
+						while (!stack.empty()) {
+							bw.write(stack.pop());
+						}
+						bw.write(" ");
+					}
+				}
 			}
-			System.out.println();
+			bw.write("\n");
 		}
-		
-		sc.close();
+		bw.flush();
 	}
 }
