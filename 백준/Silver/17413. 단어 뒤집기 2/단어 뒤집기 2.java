e5805334370b77614dee5836ab7c52144ca2aed6
jitae-moon <jitae.moon@icloud.com>
2023-02-09 20:48:52 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 272 ms, Memory: 19208 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,37 +1,42 @@
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
 		Stack<Character> stack = new Stack<>();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String str = sc.nextLine() + " ";
 		boolean tag = false;
 		
 		for (char ch: str.toCharArray()) {
 			if (ch == '<') {
 				tag = true;
-				printStack(stack);
-				System.out.print(ch);
+				printStack(stack, bw);
+				bw.write(ch);
 			} else if (ch == '>') {
 				tag = false;
-				System.out.print(ch);
+				bw.write(ch);
 			} else if (tag) {
-				System.out.print(ch);
+				bw.write(ch);
 			} else {
 				if (ch == ' ') {
-					printStack(stack);
-					System.out.print(ch);
+					printStack(stack, bw);
+					bw.write(ch);
 				} else {
 					stack.push(ch);
 				}
 			}
 		}
-		
+		bw.flush();
 	}
 	
-	static void printStack(Stack<Character> stack) {
+	static void printStack(Stack<Character> stack, BufferedWriter bw) throws IOException {
 		while (!stack.empty()) {
-			System.out.print(stack.pop());
+			bw.write(stack.pop());
 		}
 	}
 }
