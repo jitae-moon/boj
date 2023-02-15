e466ef5243033a97efc658e8de8fd6b98d2fbce9
jitae-moon <jitae.moon@icloud.com>
2023-02-15 15:08:05 +0900

    [Silver II] Title: 쇠막대기, Time: 124 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,9 +1,12 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		char[] chArr = sc.nextLine().toCharArray();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] chArr = br.readLine().toCharArray();
 		Stack<Character> stack = new Stack<>();
 		int cnt = 0;
 
