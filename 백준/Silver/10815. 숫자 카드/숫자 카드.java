0fed3c9574b8fa6c0e87df113ee29716b8749edc
jitae-moon <jitae.moon@icloud.com>
2023-06-25 10:58:31 +0900

    [Silver V] Title: 숫자 카드, Time: 4296 ms, Memory: 311928 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,21 +1,36 @@
+
+
 import java.util.*;
 
 public class Main {
+    static int check(int[] arr, int n) {
+        int lt = 0;
+        int rt = arr.length - 1;
+        int mid = 0;
+        while (lt <= rt) {
+            mid = (lt + rt) / 2;
+            if (arr[mid] > n) {
+                rt = mid - 1;
+            } else if (arr[mid] < n) {
+                lt = mid + 1;
+            } else return 1;
+        }
+        return 0;
+    }
+
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) set.add(sc.nextInt());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
         int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) arr2[i] = sc.nextInt();
+        Arrays.sort(arr);
 
-        for (int i = 0; i < m; i++) {
-            int num = sc.nextInt();
-            if (set.contains(num)) {
-                System.out.print(1 + " ");
-            } else {
-                System.out.print(0 + " ");
-            }
-        }
-
+        for (int num: arr2) System.out.print(check(arr, num) + " ");
     }
 }
