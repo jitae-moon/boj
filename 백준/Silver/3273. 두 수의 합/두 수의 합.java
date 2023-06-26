d710bc1370f43c2b5de530ef0ed8288b753e1677
jitae-moon <jitae.moon@icloud.com>
2023-06-26 15:22:51 +0900

    [Silver III] Title: 두 수의 합, Time: 848 ms, Memory: 96584 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        int x = sc.nextInt();
+
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = arr.length - 1;
+
+        int ans = 0;
+        int sum = 0;
+        while (lt < rt) {
+            sum = arr[lt] + arr[rt];
+            if (sum > x) rt--;
+            else if (sum < x) lt++;
+            else {
+                ans++;
+                lt++;
+                rt--;
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
