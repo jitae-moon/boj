b7b89834dbe294a4d37b4df3829966cfcc2436e2
jitae-moon <jitae.moon@icloud.com>
2023-06-26 18:38:28 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 12764 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,27 +1,25 @@
 
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int[] cnt = new int[11];
-
+        int ans = 0;
+        int[] cnt = new int[10];
         for (char ch: str.toCharArray()) {
             int n = ch - '0';
             if (n == 6 || n == 9) {
                 if (cnt[6] == cnt[9]) cnt[6]++;
                 else if (cnt[6] > cnt[9]) cnt[9]++;
-                else cnt[6]++;
             } else {
                 cnt[n]++;
             }
         }
-        int max = Integer.MIN_VALUE;
         for (int i: cnt) {
-            if (i > max) max = i;
+            if (i > ans) ans = i;
         }
-        System.out.println(max);
+        System.out.println(ans);
+
     }
 }
