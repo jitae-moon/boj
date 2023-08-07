29ff62a6c22e199ccbd8c5eb9e5799c28a3c1f9b
jitae-moon <jitae.moon@icloud.com>
2023-08-08 00:45:44 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 16888 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -6,26 +6,42 @@ public class Main {
     static int n;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static void bfs(Pair pair, char c, char[][] board) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(pair);
-        board[pair.x][pair.y] = 'O';
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Pair cur = queue.poll();
-                int x = cur.x;
-                int y = cur.y;
-                for (int j = 0; j < 4; j++) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == c) {
-                        queue.offer(new Pair(nx, ny));
-                        board[nx][ny] = 'O';
+//    static void bfs(Pair pair, char c, char[][] board) {
+//        Queue<Pair> queue = new LinkedList<>();
+//        queue.offer(pair);
+//        board[pair.x][pair.y] = 'O';
+//        while (!queue.isEmpty()) {
+//            int size = queue.size();
+//            for (int i = 0; i < size; i++) {
+//                Pair cur = queue.poll();
+//                int x = cur.x;
+//                int y = cur.y;
+//                for (int j = 0; j < 4; j++) {
+//                    int nx = x + dx[j];
+//                    int ny = y + dy[j];
+//                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == c) {
+//                        queue.offer(new Pair(nx, ny));
+//                        board[nx][ny] = 'O';
+//
+//                    }
+//                }
+//
+//            }
+//        }
+//
+//    }
 
-                    }
-                }
-
+    static int[][] vis;
+    static void dfs(Pair pair, char[][] board, char c) {
+        int x = pair.x;
+        int y = pair.y;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == c && vis[nx][ny] == 0) {
+                vis[nx][ny] = 1;
+                board[nx][ny] = 'O';
+                dfs(new Pair(nx, ny), board, c);
             }
         }
 
@@ -52,20 +68,30 @@ public class Main {
             }
         }
 
+        vis = new int[n][n];
         int cnt = 0, cnt2 = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] != 'O') {
-                    bfs(new Pair(i, j), board[i][j], board);
+//                    bfs(new Pair(i, j), board[i][j], board);
+                    vis[i][j] = 1;
+                    char tmp = board[i][j];
+                    board[i][j] = 'O';
+                    dfs(new Pair(i, j), board, tmp);
                     cnt++;
                 }
             }
         }
+        vis = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board2[i][j] != 'O') {
-                    bfs(new Pair(i, j), board2[i][j], board2);
+//                    bfs(new Pair(i, j), board2[i][j], board2);
+                    vis[i][j] = 1;
+                    char tmp = board2[i][j];
+                    board2[i][j] = 'O';
+                    dfs(new Pair(i, j), board2, tmp);
                     cnt2++;
                 }
             }
