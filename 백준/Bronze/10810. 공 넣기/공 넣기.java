e07a8704a1ed77d63a90bedbd9a9d2fa1633a180
jitae-moon <jitae.moon@icloud.com>
2023-10-12 21:35:27 +0900

    [Bronze III] Title: 공 넣기, Time: 80 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,18 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n+1];
 
         for (int x = 0; x < m; x++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            int k = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
             for (int y = i; y <= j; y++) {
                 arr[y] = k;
@@ -20,7 +29,9 @@ public class Main {
         }
 
         for (int i = 1; i <= n; i++) {
-            System.out.print(arr[i] + " ");
+            bw.write(arr[i] + " ");
         }
+
+        bw.flush();
     }
 }
