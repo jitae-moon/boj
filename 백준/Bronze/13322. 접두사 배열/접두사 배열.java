feacc1d3cd9feecdf7bc842a2f670d3a4fc0461e
jitae-moon <jitae.moon@icloud.com>
2023-10-17 11:34:23 +0900

    [Bronze I] Title: 접두사 배열, Time: 1640 ms, Memory: 35132 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(i);
+        }
+    }
+}
